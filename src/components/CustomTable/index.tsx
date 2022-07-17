7c1ab814ea5f5f0f2741a1c523cdfca98f85a925
Kadek Pradnyana <kadek.fwd@glints.com>
2022-07-17 21:25:20 +0800

    fetch random api and render

--- src/components/CustomTable/index.tsx
+++ src/components/CustomTable/index.tsx
@@ -3,58 +3,131 @@ import {
     Table,
     Thead,
     Tbody,
-    Tfoot,
     Tr,
     Th,
     Td,
-    TableCaption,
     TableContainer,
 } from '@chakra-ui/react'
-import SearchAndFilter from '../SearchAndFilter/SearchAndFilter';
+import SearchAndFilter, { FILTER_DEFAULT_VALUE } from '../SearchAndFilter/SearchAndFilter';
 
-const CustomTable = () => {
-    const [searchValue, setSearchValue] = React.useState();
-    const [filterValue, setFilterValue] = React.useState();
+interface RandomResponse {
+    info: {
+        page: number;
+        results: number;
+        seed: string;
+        version: string;
+    };
+    results: RandomData[];
+}
+
+export interface RandomData {
+    name: {
+        title: string;
+        first: string;
+        last: string;
+    };
+    email: string;
+    gender: string;
+    login: {
+        uuid: string;
+        username: string;
+        password: string;
+        salt: string;
+        sha1: string;
+        sha256: string;
+        md5: string;
+    },
+    registered: {
+        date: string;
+        age: number;
+    }
+}
+
+interface TableData {
+    label: string;
+    value?: string;
+    isNumeric?: boolean;
+};
+
+export interface TableItem {
+    [field: string]: TableData;
+};
+
+export interface CustomTableProps {
+    fetchEndpoint: string;
+    dataMapping: (data: RandomData) => Record<string, Record<string, any>>;
+
+    getSearchValue?: React.Dispatch<React.SetStateAction<any>> | ((value: string) => void);
+    getFilterValue?: React.Dispatch<React.SetStateAction<any>> | ((value: string) => void);
+}
+
+const CustomTable = (props: CustomTableProps) => {
+    const [users, setUsers] = React.useState<any[]>([]);
+    const [searchValue, setSearchValue] = React.useState('');
+    const [filterValue, setFilterValue] = React.useState(FILTER_DEFAULT_VALUE);
+
+    React.useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    React.useEffect(() => {
+        if (filterValue === 'all') {
+            fetchUsers();
+        } else {
+            fetchUsers(filterValue);
+        }
+    }, [filterValue]);
+
+    const _onHandleSort = (field: string) => {
+        console.log(field);
+    }
+
+    const fetchUsers = async (gender?: string) => {
+        const queryString = new URLSearchParams({
+            ...(gender ? { gender: gender } : undefined),
+            results: String(10),
+            page: String(1),
+            inc: 'name,gender,login,registered,email'
+        });
+        const response = await fetch(`${props.fetchEndpoint}?${queryString}`);
+
+        if (response.ok) {
+            const data = await response.json() as RandomResponse;
+            const transformedData = data.results.map(d => props.dataMapping(d));
+            setUsers(transformedData);
+        }
+    };
+
+    const renderDataTable = (data: TableItem[]) => {
+        const firstItem = data[0];
+        return (
+            <>
+                <Thead>
+                    <Tr>
+                        {Object.keys((firstItem)).map(d => <Th key={d} isNumeric={data[0][d].isNumeric} onClick={() => _onHandleSort(d)}>{d}</Th>)}
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {data.map((d, idx) => (
+                        <Tr key={idx}>
+                            {Object.keys(d).map(item => <Td key={item} isNumeric={d[item].isNumeric}>{d[item].value}</Td>)}
+                        </Tr>
+                    ))}
+                </Tbody>
+            </>
+        )
+    }
 
     return (
         <>
             <SearchAndFilter getSearchValue={setSearchValue} getFilterValue={setFilterValue} />
-            <TableContainer>
-                <Table variant='simple'>
-                    <TableCaption>Imperial to metric conversion factors</TableCaption>
-                    <Thead>
-                        <Tr>
-                            <Th>To convert</Th>
-                            <Th>into</Th>
-                            <Th isNumeric>multiply by</Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        <Tr>
-                            <Td>inches</Td>
-                            <Td>millimetres (mm)</Td>
-                            <Td isNumeric>25.4</Td>
-                        </Tr>
-                        <Tr>
-                            <Td>feet</Td>
-                            <Td>centimetres (cm)</Td>
-                            <Td isNumeric>30.48</Td>
-                        </Tr>
-                        <Tr>
-                            <Td>yards</Td>
-                            <Td>metres (m)</Td>
-                            <Td isNumeric>0.91444</Td>
-                        </Tr>
-                    </Tbody>
-                    <Tfoot>
-                        <Tr>
-                            <Th>To convert</Th>
-                            <Th>into</Th>
-                            <Th isNumeric>multiply by</Th>
-                        </Tr>
-                    </Tfoot>
-                </Table>
-            </TableContainer>
+            {users?.length ? (
+                <TableContainer>
+                    <Table variant='simple'>
+                        {renderDataTable(users)}
+                    </Table>
+                </TableContainer>
+            ) : <p>Table wrongly initiated.</p>}
         </>
     )
 }
