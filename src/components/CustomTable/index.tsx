4bd09575b78af46cdda1418bb1fe62eb17b88aa9
Kadek Pradnyana <kadek.fwd@glints.com>
2022-07-18 11:16:30 +0800

    refactoring

--- src/components/CustomTable/index.tsx
+++ src/components/CustomTable/index.tsx
@@ -7,39 +7,6 @@ import * as Rambda from 'rambda';
 import PaginationButton from '../PaginationButton';
 import TableWrapper from '../TableWrapper';
 
-interface RandomResponse {
-    info: {
-        page: number;
-        results: number;
-        seed: string;
-        version: string;
-    };
-    results: RandomData[];
-}
-
-export interface RandomData {
-    name: {
-        title: string;
-        first: string;
-        last: string;
-    };
-    email: string;
-    gender: string;
-    login: {
-        uuid: string;
-        username: string;
-        password: string;
-        salt: string;
-        sha1: string;
-        sha256: string;
-        md5: string;
-    },
-    registered: {
-        date: string;
-        age: number;
-    }
-}
-
 export enum Sorting {
     DESCENDING = 'DESCENDING',
     ASCENDING = 'ASCENDING'
@@ -47,101 +14,94 @@ export enum Sorting {
 
 export interface CustomTableProps {
     fetchEndpoint: string;
-    dataMapping: (data: RandomData) => Record<string, Record<string, any>>;
+    dataMapping: (response: any) => Record<string, Record<string, any>>[];
 
     getSearchValue?: React.Dispatch<React.SetStateAction<any>> | ((value: string) => void);
     getFilterValue?: React.Dispatch<React.SetStateAction<any>> | ((value: string) => void);
 }
 
-const fetchAPI = async (endpoint: string, page: string, perPage: string, options: Record<string, string | undefined>) => {
+const fetchAPI = async (endpoint: string, page: string, pageSize: string, options: Record<string, string | undefined>) => {
     const queryString = new URLSearchParams({
         ...options,
-        results: perPage,
-        page: page
+        results: pageSize,
+        page: page,
+        pageSize: pageSize
     });
     const response = await fetch(`${endpoint}?${queryString}`);
 
     if (response.ok) {
-        return await response.json() as RandomResponse;
+        return await response.json();
     }
 };
-
-const findColumns = (columns: any[], value: string) => columns.map(field => {
-    const searchFields = Object.keys(field).filter(f =>
-        field[f].isSearchable
-        && field[f].value
-            .toString()
-            .toLowerCase()
-            .includes(value.toLowerCase())
-    );
-    return searchFields.length ? field : undefined;
-}).filter(x => x);
 
 const ROWS_SIZE = 5123;
 const ROWS_PER_PAGE = 10;
 const PAGE_LENGTH = Math.ceil(ROWS_SIZE / ROWS_PER_PAGE);
 
+const sortByPath = Rambda.pipe(Rambda.path, Rambda.sortBy);
+
 const CustomTable = (props: CustomTableProps) => {
     const [columns, setColumns] = React.useState<any[]>([]);
     const [filteredColumns, setFilteredColumns] = React.useState<any[]>([]);
+    const [sort, setSort] = React.useState<{ by: string, order: Sorting }>();
     const [page, setPage] = React.useState(1);
     const [searchValue, setSearchValue] = React.useState(null);
     const [filterValue, setFilterValue] = React.useState(FILTER_DEFAULT_VALUE);
 
     React.useEffect(() => {
         if (filterValue === 'all') {
-            fetchUsers(page);
+            fetchData(page);
         } else {
-            fetchUsers(page, filterValue);
+            fetchData(page, filterValue, searchValue);
         }
     }, [filterValue]);
 
     React.useEffect(() => {
         if (searchValue !== null) {
             setTimeout(() => {
-                searchColumns(searchValue);
+                fetchData(page, filterValue, searchValue);
             }, 750);
         }
     }, [searchValue]);
 
-    const fetchUsers = async (page: number, gender?: string) => {
+    const fetchData = async (page: number, gender?: string, searchValue?: string | null, sort?: { by: string; order: Sorting }) => {
         const pageNumberFloor = Math.floor(ROWS_SIZE / ROWS_PER_PAGE);
         const offset = page > pageNumberFloor ? (ROWS_SIZE / ROWS_PER_PAGE).toString().split('.')[1][0] : String(ROWS_PER_PAGE);
-        const data = await fetchAPI(props.fetchEndpoint, String(page), offset, { gender });
+        const options = {
+            ...(gender && gender !== FILTER_DEFAULT_VALUE ? { gender } : undefined),
+            ...(searchValue ? { keyword: searchValue } : undefined),
+            ...(sort ? { sortBy: sort.by, sortOrder: sort.order.toLowerCase() } : undefined)
+        };
+        const data = await fetchAPI(props.fetchEndpoint, String(page), offset, options);
         if (data) {
-            const transformedData = data.results.map(d => props.dataMapping(d));
+            const transformedData = props.dataMapping(data);
             setColumns(transformedData);
-            setFilteredColumns(transformedData);
-        }
-    }
-
-    const searchColumns = async (value: string) => {
-        if (value === '') {
-            setFilteredColumns(columns);
-        } else {
-            const searchableFields = findColumns(columns, value);
-            setFilteredColumns(searchableFields);
+            if (sort) {
+                const sortAscending = sortByPath([sort.by, 'value']);
+                const sortDescending = Rambda.pipe(sortAscending as any, Rambda.reverse);
+                const result = sort.order === Sorting.DESCENDING ? sortDescending(columns) as unknown as any[] : sortAscending(columns);
+                const sortedColumns: any[] = Rambda.assocPath([0, sort.by, 'sorting'], sort.order, result);
+                setFilteredColumns(sortedColumns);
+            } else {
+                setFilteredColumns(transformedData);
+            }
         }
     }
 
     const _handleSort = (field: string, sorting: Sorting) => {
-        const sortByPath = Rambda.pipe(Rambda.path, Rambda.sortBy);
-        const sortAscending = sortByPath([field, 'value']);
-        const sortDescending = Rambda.pipe(sortAscending as any, Rambda.reverse);
-        const result = sorting === Sorting.DESCENDING ? sortDescending(columns) as unknown as any[] : sortAscending(columns);
-        const sortedColumns: any[] = Rambda.assocPath([0, field, 'sorting'], sorting, result);
-        setFilteredColumns(sortedColumns);
+        setSort({ by: field, order: sorting });
+        fetchData(page, filterValue, searchValue, sort);
     }
 
     const _handlePagination = (page: number) => {
         setPage(page);
-        fetchUsers(page, filterValue);
+        fetchData(page, filterValue, searchValue, sort);
     }
 
     const _handlePreviousPage = () => {
         setPage(prev => {
             prev--;
-            fetchUsers(prev, filterValue);
+            fetchData(page, filterValue, searchValue, sort);
             return prev;
         });
     }
@@ -149,7 +109,7 @@ const CustomTable = (props: CustomTableProps) => {
     const _handleNextPage = () => {
         setPage(prev => {
             prev++;
-            fetchUsers(prev, filterValue);
+            fetchData(page, filterValue, searchValue, sort);
             return prev;
         });
     }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,39 @@
 import React from 'react';
-import CustomTable, { RandomData } from '../components/CustomTable';
-import { Container } from '@chakra-ui/react'
+import CustomTable from '../components/CustomTable';
+import { Container } from '@chakra-ui/react';
+
+interface RandomResponse {
+  info: {
+    page: number;
+    results: number;
+    seed: string;
+    version: string;
+  };
+  results: RandomData[];
+}
+
+export interface RandomData {
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  email: string;
+  gender: string;
+  login: {
+    uuid: string;
+    username: string;
+    password: string;
+    salt: string;
+    sha1: string;
+    sha256: string;
+    md5: string;
+  },
+  registered: {
+    date: string;
+    age: number;
+  }
+}
 
 const constructTableItem = (label: string, value: string, isSearchable?: boolean, isSortable: boolean = true, isNumeric?: boolean) => ({
   label,
@@ -11,15 +44,17 @@ const constructTableItem = (label: string, value: string, isSearchable?: boolean
   sorting: 'ASCENDING'
 });
 
-const _dataMapping = (data: RandomData) => {
-  const { login, registered, name, ...rest } = data;
-  return {
-    username: constructTableItem('Username', login.username, true, false),
-    name: constructTableItem('Name', `${name.first} ${name.last}`, true),
-    email: constructTableItem('Email', rest.email, true),
-    gender: constructTableItem('Gender', rest.gender),
-    registered: constructTableItem('Registered Date', new Date(registered.date).toLocaleString())
-  }
+const _dataMapping = (response: RandomResponse) => {
+  return response.results.map(data => {
+    const { login, registered, name, ...rest } = data;
+    return {
+      username: constructTableItem('Username', login.username, true, false),
+      name: constructTableItem('Name', `${name.first} ${name.last}`, true),
+      email: constructTableItem('Email', rest.email, true),
+      gender: constructTableItem('Gender', rest.gender),
+      registered: constructTableItem('Registered Date', new Date(registered.date).toLocaleString())
+    }
+  });
 }
 
 const IndexPage = () => {
