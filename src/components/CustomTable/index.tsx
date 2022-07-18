7ec278fb9ce857d2204590381e94b05f09194f79
Kadek Pradnyana <kadek.fwd@glints.com>
2022-07-18 09:41:14 +0800

    implement sorting and pagination

--- src/components/CustomTable/index.tsx
+++ src/components/CustomTable/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import {
+    Button,
+    Flex,
+    IconButton,
+    Stack,
+    Spacer,
     Table,
     Thead,
     Tbody,
@@ -7,8 +12,10 @@ import {
     Th,
     Td,
     TableContainer,
-} from '@chakra-ui/react'
+} from '@chakra-ui/react';
+import { ChevronUpIcon, ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import SearchAndFilter, { FILTER_DEFAULT_VALUE } from '../SearchAndFilter/SearchAndFilter';
+import * as Rambda from 'rambda';
 
 interface RandomResponse {
     info: {
@@ -43,11 +50,18 @@ export interface RandomData {
     }
 }
 
+export enum Sorting {
+    DESCENDING = 'DESCENDING',
+    ASCENDING = 'ASCENDING'
+}
+
 interface TableData {
     label: string;
     isSearchable: boolean;
+    isSortable: boolean;
     value?: string;
     isNumeric?: boolean;
+    sorting: Sorting;
 };
 
 export interface TableItem {
@@ -62,11 +76,11 @@ export interface CustomTableProps {
     getFilterValue?: React.Dispatch<React.SetStateAction<any>> | ((value: string) => void);
 }
 
-const fetchAPI = async (endpoint: string, options: Record<string, string | undefined>) => {
+const fetchAPI = async (endpoint: string, page: string, perPage: string, options: Record<string, string | undefined>) => {
     const queryString = new URLSearchParams({
         ...options,
-        results: String(10),
-        page: String(1)
+        results: perPage,
+        page: page
     });
     const response = await fetch(`${endpoint}?${queryString}`);
 
@@ -86,22 +100,41 @@ const findColumns = (columns: any[], value: string) => columns.map(field => {
     return searchFields.length ? field : undefined;
 }).filter(x => x);
 
-const TableContent = ({ data }: { data: TableItem[] }) => {
-    const firstItem = data[0];
+const TableContent = (props: { data: TableItem[], handleSort: (field: string, sorting: Sorting) => void }) => {
+    const firstItem = props.data[0];
 
-    const _onHandleSort = (field: string) => {
-        console.log(field);
+    const _onHandleSort = (field: string, sorting: Sorting) => {
+        console.log(field, sorting);
+        props.handleSort(field, sorting === Sorting.ASCENDING ? Sorting.DESCENDING : Sorting.ASCENDING);
     }
 
     return (
         <>
             <Thead>
                 <Tr>
-                    {Object.keys((firstItem)).map(d => <Th key={d} isNumeric={data[0][d].isNumeric} onClick={() => _onHandleSort(d)}>{d}</Th>)}
+                    {Object.keys((firstItem)).map(d =>
+                        <Th
+                            key={d}
+                            isNumeric={firstItem[d].isNumeric}
+                            cursor={firstItem[d].isSortable ? 'pointer' : undefined}
+                            onClick={firstItem[d].isSortable ? () => _onHandleSort(d, firstItem[d].sorting) : undefined}
+                        >
+                            <Flex minWidth='max-content'>
+                                {firstItem[d].label}
+                                <Spacer />
+                                {firstItem[d].isSortable && (
+                                    <Stack spacing={-1}>
+                                        <ChevronUpIcon color={firstItem[d].sorting === Sorting.DESCENDING ? 'gray.200' : undefined} />
+                                        <ChevronDownIcon color={firstItem[d].sorting === Sorting.ASCENDING ? 'gray.200' : undefined} />
+                                    </Stack>
+                                )}
+                            </Flex>
+                        </Th>
+                    )}
                 </Tr>
             </Thead>
             <Tbody>
-                {data.map((d, idx) => (
+                {props.data.map((d, idx) => (
                     <Tr key={idx}>
                         {Object.keys(d).map(item => <Td key={item} isNumeric={d[item].isNumeric}>{d[item].value}</Td>)}
                     </Tr>
@@ -111,27 +144,89 @@ const TableContent = ({ data }: { data: TableItem[] }) => {
     )
 }
 
-const TableWrapper = ({ columns }: { columns: any[] }) => {
-    return columns?.length ? (
+const TableWrapper = (props: { columns: any[], handleSort: (field: string, sorting: Sorting) => void }) => {
+    return props.columns?.length ? (
         <TableContainer>
             <Table variant='simple'>
-                <TableContent data={columns} />
+                <TableContent data={props.columns} handleSort={props.handleSort} />
             </Table>
         </TableContainer>
     ) : <p>No data found.</p>;
 }
 
+const ROWS_SIZE = 5123;
+const ROWS_PER_PAGE = 10;
+const PAGE_LENGTH = Math.ceil(ROWS_SIZE / ROWS_PER_PAGE);
+
+const PageButton = (props: {
+    page: number;
+    pageNumber: number;
+    handlePaginateButton: (pageNumber: number) => void;
+}) => <Button size='sm' isActive={props.page === props.pageNumber} onClick={() => props.handlePaginateButton(props.pageNumber)}>{props.pageNumber}</Button>;
+
+const PaginateButton = (props: {
+    page: number,
+    handlePaginateButton: (pageNumber: number) => void;
+}) => {
+    return (
+        <>
+            {Array.from({ length: PAGE_LENGTH }, (v, i) => i + 1).map((pageNumber) => {
+                if (PAGE_LENGTH > 10) {
+                    if (pageNumber <= 6 || pageNumber >= PAGE_LENGTH - 4) {
+                        return <PageButton key={pageNumber} page={props.page} pageNumber={pageNumber} handlePaginateButton={props.handlePaginateButton} />;
+                    }
+
+                    if (pageNumber === 7) {
+                        return <p>...</p>
+                    }
+                } else {
+                    return <PageButton key={pageNumber} page={props.page} pageNumber={pageNumber} handlePaginateButton={props.handlePaginateButton} />;
+                }
+            })}
+        </>
+    );
+}
+
+const PaginationButton = (props: {
+    page: number;
+    handlePreviousPage: () => void;
+    handleNextPage: () => void;
+    handlePaginateButton: (pageNumber: number) => void
+}) => {
+    return (
+        <Flex minWidth='max-content'>
+            <p>Page <b>{props.page}</b> of {PAGE_LENGTH}</p>
+            <Spacer />
+            <Stack spacing={1} direction='row' align='center'>
+                <IconButton
+                    aria-label='Previous page'
+                    isActive={props.page <= 1}
+                    size='sm'
+                    onClick={props.page > 1 ? props.handlePreviousPage : undefined} icon={<ChevronLeftIcon />}
+                />
+                <PaginateButton page={props.page} handlePaginateButton={props.handlePaginateButton} />
+                <IconButton
+                    aria-label='Next page'
+                    isActive={props.page >= PAGE_LENGTH}
+                    size='sm' onClick={props.page < PAGE_LENGTH ? props.handleNextPage : undefined}
+                    icon={<ChevronRightIcon />} />
+            </Stack>
+        </Flex>
+    )
+}
+
 const CustomTable = (props: CustomTableProps) => {
     const [columns, setColumns] = React.useState<any[]>([]);
     const [filteredColumns, setFilteredColumns] = React.useState<any[]>([]);
+    const [page, setPage] = React.useState(1);
     const [searchValue, setSearchValue] = React.useState(null);
     const [filterValue, setFilterValue] = React.useState(FILTER_DEFAULT_VALUE);
 
     React.useEffect(() => {
         if (filterValue === 'all') {
-            fetchUsers();
+            fetchUsers(page);
         } else {
-            fetchUsers(filterValue);
+            fetchUsers(page, filterValue);
         }
     }, [filterValue]);
 
@@ -143,8 +238,10 @@ const CustomTable = (props: CustomTableProps) => {
         }
     }, [searchValue]);
 
-    const fetchUsers = async (gender?: string) => {
-        const data = await fetchAPI(props.fetchEndpoint, { gender });
+    const fetchUsers = async (page: number, gender?: string) => {
+        const pageNumberFloor = Math.floor(ROWS_SIZE / ROWS_PER_PAGE);
+        const offset = page > pageNumberFloor ? (ROWS_SIZE / ROWS_PER_PAGE).toString().split('.')[1][0] : String(ROWS_PER_PAGE);
+        const data = await fetchAPI(props.fetchEndpoint, String(page), offset, { gender });
         if (data) {
             const transformedData = data.results.map(d => props.dataMapping(d));
             setColumns(transformedData);
@@ -161,11 +258,47 @@ const CustomTable = (props: CustomTableProps) => {
         }
     }
 
+    const _handleSort = (field: string, sorting: Sorting) => {
+        const sortByPath = Rambda.pipe(Rambda.path, Rambda.sortBy);
+        const sortAscending = sortByPath([field, 'value']);
+        const sortDescending = Rambda.pipe(sortAscending as any, Rambda.reverse);
+        const result = sorting === Sorting.DESCENDING ? sortDescending(columns) as unknown as any[] : sortAscending(columns);
+        const sortedColumns: any[] = Rambda.assocPath([0, field, 'sorting'], sorting, result);
+        setFilteredColumns(sortedColumns);
+    }
+
+    const _handlePagination = (page: number) => {
+        setPage(page);
+        fetchUsers(page, filterValue);
+    }
+
+    const _handlePreviousPage = () => {
+        setPage(prev => {
+            prev--;
+            fetchUsers(prev, filterValue);
+            return prev;
+        });
+    }
+
+    const _handleNextPage = () => {
+        setPage(prev => {
+            prev++;
+            fetchUsers(prev, filterValue);
+            return prev;
+        });
+    }
+
     return (
-        <>
+        <Stack direction='column' spacing={4}>
             <SearchAndFilter getSearchValue={setSearchValue} getFilterValue={setFilterValue} />
-            <TableWrapper columns={filteredColumns} />
-        </>
+            <TableWrapper columns={filteredColumns} handleSort={_handleSort} />
+            <PaginationButton
+                page={page}
+                handlePaginateButton={(pageNumber) => _handlePagination(pageNumber)}
+                handlePreviousPage={() => _handlePreviousPage()}
+                handleNextPage={() => _handleNextPage()}
+            />
+        </Stack>
     )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import CustomTable from '../components/CustomTable';
+import CustomTable, { RandomData } from '../components/CustomTable';
 import { Container } from '@chakra-ui/react'
+
+const constructTableItem = (label: string, value: string, isSearchable?: boolean, isSortable: boolean = true, isNumeric?: boolean) => ({
+  label,
+  value,
+  isNumeric,
+  isSearchable,
+  isSortable,
+  sorting: 'ASCENDING'
+});
+
+const _dataMapping = (data: RandomData) => {
+  const { login, registered, name, ...rest } = data;
+  return {
+    username: constructTableItem('Username', login.username, true, false),
+    name: constructTableItem('Name', `${name.first} ${name.last}`, true),
+    email: constructTableItem('Email', rest.email, true),
+    gender: constructTableItem('Gender', rest.gender),
+    registered: constructTableItem('Registered Date', new Date(registered.date).toLocaleString())
+  }
+}
 
 const IndexPage = () => {
   return (
-    <div>
-      <Container maxW='1200px'>
-        <CustomTable />
-      </Container>
-    </div>
+    <Container maxW='1200px'>
+      <CustomTable
+        fetchEndpoint='https://randomuser.me/api'
+        dataMapping={_dataMapping}
+      />
+    </Container>
   );
 };
 
